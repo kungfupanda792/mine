91dccc8fdd30621b96767622abef97fd82c7a79e
kungfupanda792 <35396929+kungfupanda792@users.noreply.github.com>
2019-09-24 18:39:57 +0530

    Adding 2md project file

--- src/main/java/com/test/hello/HelloApplication.java
+++ src/main/java/com/test/hello/HelloApplication.java
@@ -1,10 +1,20 @@
 package com.test.hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+ 
+@EnableAutoConfiguration
 public class HelloApplication {
+@RequestMapping("/")
+String home() {
+	return "Hello World";
+}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
--- src/test/java/com/test/hello/HelloApplicationTests.java
+++ src/test/java/com/test/hello/HelloApplicationTests.java
@@ -1,7 +1,10 @@
 package com.test.hello;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +14,13 @@ public class HelloApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	@Autowired
+	public HelloApplication hm;
+	@Test
+	public void test() {
+		String sr=hm.home();
+		assertEquals(sr,"Hello World");
 	}
 
 }
